77118716abca2a799aa5d4ae37e4cf5a79d3ee8e
Mahadev Gaonkar <mahadev.gaonkar@gmail.com>
2019-10-20 00:27:57 +0530

    Initial Commit

---
+++ rock-finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rock-finder
+//
+//  Created by Mahadev Gaonkar on 20/10/19.
+//  Copyright © 2019 Mahadev Gaonkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
